11f0f717187356995fde7c9fba8437d760248f8e
HONEY2614 <155128736+HONEY2614@users.noreply.github.com>
2023-12-29 12:18:32 +0530

    Add files via upload

---
+++ Biggest.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Biggest
+{
+	public static void main(String[] args)
+	{
+		Scanner s=new Scanner(System.in);
+		int a,b;
+		System.out.println("Enter Two Nos");
+		a=s.nextInt();
+		b=s.nextInt();
+		if(a>b)
+		{
+			System.out.println(a+"is Biggest");
+		}
+		else
+		{
+			System.out.println(b+"is the Biggest");
+		}
+	}
+}
+
+
+		
+
+
+
+
