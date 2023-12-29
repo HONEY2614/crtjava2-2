5d3a39e8764b3b719ca07579ad00b16fa87d8952
HONEY2614 <155128736+HONEY2614@users.noreply.github.com>
2023-12-29 15:57:28 +0530

    Add files via upload

---
+++ Leap.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Leap
+{
+	public static void main(String[] args)
+	{
+		Scanner s=new Scanner(System.in);
+		int y;
+		System.out.println("Enter a Year");
+		y=s.nextInt();
+		if(y%4==0 && (y%100!=0|| y%400==0))
+		{
+			System.out.println("Leap Year");
+		}
+		else
+		{
+			System.out.println("Not a leap year");
+		}
+		
+
+	}
+}
+
+
+		
+
+		
+
+
+
+
