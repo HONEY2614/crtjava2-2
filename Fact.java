22ef4a3cbcf1f520a6981f9011c673370721db39
HONEY2614 <155128736+HONEY2614@users.noreply.github.com>
2023-12-30 16:41:25 +0530

    Add files via upload

---
+++ Fact.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Fact
+
+{
+    
+public static void main(String[] args)
+    
+{
+       
+ Scanner s=new Scanner(System.in); 
+
+ int n,r=1;
+ 
+ System.out.println("Enter n Value");
+
+ n=s.nextInt();
+	    
+while (n>=1)
+	   
+{
+	        
+ r=r*n;
+	         
+n=n-1;
+	    
+ }
+	     
+System.out.println(r);
+      
+    
+}
+
+}
+
+
+
+	   
+	   
+		
