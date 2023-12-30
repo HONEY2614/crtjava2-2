35e2db047f53c1cdb89997fe956c906fb649d387
HONEY2614 <155128736+HONEY2614@users.noreply.github.com>
2023-12-30 14:34:07 +0530

    Add files via upload

---
+++ Art.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+class Art
+{
+	
+public static void main(String[] args)
+ 
+	
+{
+	    
+Scanner s=new Scanner(System.in);
+	    
+char ch;
+	    
+ch=s.next().charAt(0);
+	    
+switch(ch)
+	    
+{
+	        
+case 'a':
+	       
+case 'e':
+	        
+case 'i':
+	        
+case 'o':
+	        
+case 'u':
+	        
+case 'A':
+	        
+case 'E':
+	        
+case 'I':
+	        
+case 'O':
+	       
+case 'U':
+	        
+System.out.println("Vowel");
+	        
+break;
+	     
+default:
+	    
+ {
+	         
+System.out.println("Consonant");
+	     
+}
+
+}
+		
+	
+}
+
+}
