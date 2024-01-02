69cf35ddf158b6de454cb958f3b94a36c9196e4d
HONEY2614 <155128736+HONEY2614@users.noreply.github.com>
2024-01-02 13:30:48 +0530

    Add files via upload

---
+++ Even2.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Even2
+
+{
+
+public static void main(String[] args) 
+	
+{
+
+Scanner s=new Scanner(System.in);	    
+int i,n;
+	    
+n=s.nextInt();
+	    
+for(i=2;i<=n;i=i+2)	    
+{
+	        
+System.out.println(i+" ");
+	   
+}
+	   
+
+}
+}
