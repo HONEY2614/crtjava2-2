2138529a48e87cc3bf7929d080fb0035a9726b8f
HONEY2614 <155128736+HONEY2614@users.noreply.github.com>
2024-01-02 13:32:33 +0530

    Add files via upload

---
+++ OddNum.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class OddNum
+{
+    
+public static void main(String[] args)
+
+    {
+        
+Scanner s=new Scanner(System.in); 
+   
+     int i=1,n;
+        
+System.out.println("Enter n Value");
+      
+ n=s.nextInt();
+	    
+while (i<=n)
+
+{
+	         
+System.out.print(i+" ");
+	          
+i=i+2;
+	     
+}
+      
+    
+}
+
+}
