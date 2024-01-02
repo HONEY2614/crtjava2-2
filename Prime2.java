2beae54bf7bb52b3189fe86734e54a19b3cb99cf
HONEY2614 <155128736+HONEY2614@users.noreply.github.com>
2024-01-02 16:39:14 +0530

    Add files via upload

---
+++ Prime2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Prime2
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+int i,n,c=0;
+n=s.nextInt();
+for(i=1;i<=n;i++)
+{
+if(n%i==0)
+c=c+1;
+}
+System.out.println(c);
+if(c==2)
+System.out.println(n+" is prime no");
+else
+System.out.println(n+"is not a prime no");
+}
+}
