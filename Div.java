71fec31a9dcc5ae3eb27a306619e76489db953dc
HONEY2614 <155128736+HONEY2614@users.noreply.github.com>
2024-01-03 16:04:58 +0530

    Add files via upload

--- Div.java
+++ Div.java
@@ -1,22 +1,29 @@
 import java.util.*;
 class Div
 {
+}
+{
 	public static void main(String[] args)
 	{
 		Scanner s=new Scanner(System.in);
-		int a,b,r;
+		int a,b;
 		System.out.println("Enter Two Nos");
 		a=s.nextInt();
 		b=s.nextInt();
-		if(b==0)
-	{
-			System.out.println("Second no cannot be zero");
-			b=1;
-	}
-	r=a/b;
-	System.out.println("Division="+r);
+		if(a>b)
+		{
+			System.out.println(a+"is Biggest);
+		}
+		else
+		{
+			System.out.println(b+"is the Biggest);
+		}
 	}
 }
+
+
+		
+
 
 
 
