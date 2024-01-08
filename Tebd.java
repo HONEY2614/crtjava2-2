66a90ff9718516d19aaca7df24f8c2df1ec203c0
HONEY2614 <155128736+HONEY2614@users.noreply.github.com>
2024-01-08 16:26:28 +0530

    Add files via upload

---
+++ Tebd.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Tebd
+{
+	public static void main(String args[])
+	{
+		Scanner s=new Scanner(System.in);
+		int a,b,r;
+		System.out.println("ENTER TWO NUMBERS");
+		a=s.nextInt();
+		b=s.nextInt();
+		r=a/b;
+		System.out.println("DIVISION="+r);
+	}
+}
+
+		
