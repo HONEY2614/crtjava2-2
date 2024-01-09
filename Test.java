54a5bc5f1356cf518e2f4f2a468f0afe78f4d232
HONEY2614 <155128736+HONEY2614@users.noreply.github.com>
2024-01-09 14:49:47 +0530

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,24 +1,16 @@
-abstract class Parent
-{
-	void show()
-	{
-		System.out.println("SHOW METHOD IN PARENT CLASS");
-}
-public abstract void m1();
-}
-class Child extends Parent
-{
-	public void m1()
-{
-		System.out.println("m1 ABSTRACT METHODS IN CHILD CLASS");
-}
-}
+import java.io.*;
+import java.util.*;
 class Test
 {
-	public static void main(String args[])
-{
-		Child c1=new Child();
-		c1.show();
-		c1.m1();
-}
+	public static void main(String args[])throws Exception
+	{
+		int a,b,c;
+		Properties p=new Properties();
+		FileInputStream f1=new FileInputStream("Data.java");
+		p.load(f1);
+		a=Integer.parseInt(p.getProperty("num1"));
+		b=Integer.parseInt(p.getProperty("num2"));
+		c=a+b;
+		System.out.println(c);
+	}
 }
